25df0049687b922d035bb31a54f62960e03363f0
Benjamin Heath <bmheath@student.fullsail.edu>
2026-07-02 13:47:18 -0400

    initial commit 2

--- server.js
+++ server.js
@@ -3,10 +3,23 @@ const { createServer } = require("node:http");
 const hostname = "127.0.0.1";
 const port = 3000;
 
+const todos = [
+  { id: 1, task: "Task one" },
+  { id: 2, task: "Task two" },
+  { id: 3, task: "Task three" },
+];
+
 const server = createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+  res.setHeader("Content-Type", "application/json");
+
+  res.end(
+    JSON.stringify({
+      success: true,
+      method: req.method,
+      data: todos,
+    }),
+  );
 });
 
 server.listen(port, hostname, () => {
